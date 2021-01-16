f2c70d45f67663516d9adadbf366c8e69df8bfa5
zed <zed@mail.ru>
2021-01-16 16:32:48 +0300

    added routes ro reviews

--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,8 @@ app_name = 'courses'
 
 
 urlpatterns = [
-    path("", views.ListCourse.as_view(), name="course_list")
-
+    path("", views.Course.as_view(), name="course_list"),
+    path("<int:course_pk>/reviews/", views.Review.as_view(), name="review_list"),
+    path("<int:course_pk>/reviews/<int:review_pk>", views.ReviewDetail.as_view(), name="review"),
+    path("<int:pk>", views.CourseDetail.as_view(), name="course"),
 ]
